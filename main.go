60ef47dc2ac53ce7e68453b089ceb66b359b0de2
Jon Bodner <jon@bodnerfamily.com>
2020-12-30 03:31:11 +0000

    make sure servers start before clients try to connect

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"context"
 	"os"
+	"sync"
 )
 
 func main() {
-	go slowServer()
-	go errServer()
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go slowServer(&wg)
+	go errServer(&wg)
+	wg.Wait()
 
 	ctx := context.Background()
 	callBoth(ctx, os.Args[1])
--- servers.go
+++ servers.go
@@ -1,22 +1,33 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net"
 	"net/http"
+	"sync"
 	"time"
 )
 
-func slowServer() {
+func slowServer(wg *sync.WaitGroup) {
 	s := &http.Server{
 		Addr: ":8080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			time.Sleep(2 * time.Second)
 			w.Write([]byte("Slow response"))
 		}),
+		// this is a trick to let us know that the server has finished launching.
+		// if we don't do this, the servers might start after the clients attempt
+		// to connect to them, causing an error.
+		BaseContext: func(net.Listener) context.Context {
+			defer wg.Done()
+			return context.Background()
+		},
 	}
-	s.ListenAndServe()
+	log.Fatal(s.ListenAndServe())
 }
 
-func errServer() {
+func errServer(wg *sync.WaitGroup) {
 	s := &http.Server{
 		Addr: ":9090",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -26,6 +37,13 @@ func errServer() {
 			}
 			w.Write([]byte("ok"))
 		}),
+		// this is a trick to let us know that the server has finished launching.
+		// if we don't do this, the servers might start after the clients attempt
+		// to connect to them, causing an error.
+		BaseContext: func(net.Listener) context.Context {
+			defer wg.Done()
+			return context.Background()
+		},
 	}
-	s.ListenAndServe()
+	log.Fatal(s.ListenAndServe())
 }
